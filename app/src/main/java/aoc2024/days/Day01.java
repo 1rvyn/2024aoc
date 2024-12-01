1dcddcbd19b9c13f5bf61e5f6e47fdce1067b333
1rvyn <irvynhall@gmail.com>
2024-12-01 12:52:48 -0500

    feat: solved part 1

--- app/src/main/java/aoc2024/days/Day01.java
+++ app/src/main/java/aoc2024/days/Day01.java
@@ -1,20 +1,45 @@
 package aoc2024.days;
 
 import aoc2024.utils.InputReader;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 {
     public static int solvePart1() {
         List<String> input = InputReader.readLines("day01/input.txt");
-        for (String line: input) {
-            System.out.println(line);
+  
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>(); 
 
-            if (line.length() > 10) {
-                System.out.println("10");
+        for (String line: input) {
+            // get first 5 
+            Integer num1 = Integer.parseInt(line.substring(0, 5));
+            Integer num2 = Integer.parseInt(line.substring(8, line.length()));
+
+            // Insert num1 into left in ascending order
+            int leftIndex = 0;
+            while (leftIndex < left.size() && left.get(leftIndex) < num1) {
+                leftIndex++;
             }
+            left.add(leftIndex, num1);
+
+            // Insert num2 into right in ascending order
+            int rightIndex = 0;
+            while (rightIndex < right.size() && right.get(rightIndex) < num2) {
+                rightIndex++;
+            }
+            right.add(rightIndex, num2);
         }
-        return 0;
-    }
+        
+        Integer total = 0;
+        for (int i = 0; i < left.size(); i++){
+            total += Math.abs(left.get(i) - right.get(i));
+        }
+
+        return total;
+
+        }
 
     public static int solvePart2() {
         List<String> input = InputReader.readLines("day01/input.txt");
