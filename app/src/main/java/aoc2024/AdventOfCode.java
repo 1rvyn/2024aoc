2b6d8fe009017cda61fd60cb9c12ea47d2ee975d
1rvyn <irvynhall@gmail.com>
2024-12-04 17:40:28 -0500

    feat: init day 3 :)

--- app/src/main/java/aoc2024/AdventOfCode.java
+++ app/src/main/java/aoc2024/AdventOfCode.java
@@ -1,8 +1,6 @@
 package aoc2024;
 
-import aoc2024.days.Day01;
-import aoc2024.days.Day02;
-
+import aoc2024.days.*;
 public class AdventOfCode {
     public static void main(String[] args) {
         System.out.println("Day 1 Part 1: " + Day01.solvePart1());
@@ -10,5 +8,8 @@ public class AdventOfCode {
 
         System.out.println("Day 2 Part 1: " + Day02.solvePart1());
         System.out.println("Day 2 Part 2: " + Day02.solvePart2());
+
+        System.out.println("Day 3 Part 1: " + Day03.solvePart1());
+        System.out.println("Day 3 Part 2: " + Day03.solvePart2());
     }
 }
---
+++ app/src/main/java/aoc2024/days/Day03.java
@@ -0,0 +1,11 @@
+package aoc2024.days;
+
+public class Day03 {
+        public static int solvePart1() {
+        return 0;
+    }
+
+    public static int solvePart2() {
+        return 0;
+    }
+}
